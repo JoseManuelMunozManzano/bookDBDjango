1ee4097fdf490f9f9203b1d60ee8460cc8644499
Jose Manuel Munoz <Neil_Mercury@yahoo.es>
2018-04-22 10:47:48 +0200

    Modified templates to display books from database

--- librosBD/views.py
+++ librosBD/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Book
 
 def book_list(request):
-	return render(request, 'librosBD/book_list.html', {})
+	books = Book.objects.order_by('title')
+	return render(request, 'librosBD/book_list.html', {'books': books})
