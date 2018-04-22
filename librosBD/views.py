94bf4a2896d7f8011bd3ef682be97ee863189952
Jose Manuel Munoz <Neil_Mercury@yahoo.es>
2018-04-22 19:38:56 +0200

    Added views to create/edit book inside the site

--- librosBD/views.py
+++ librosBD/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Book
 
 def book_list(request):
 	books = Book.objects.order_by('title')
 	return render(request, 'librosBD/book_list.html', {'books': books})
+
+def book_detail(request, pk):
+	book = get_object_or_404(Book, pk=pk)
+	return render(request, 'librosBD/book_detail.html', {'book': book})
