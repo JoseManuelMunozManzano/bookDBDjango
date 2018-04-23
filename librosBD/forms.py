e99a1dc91c6be13cc65175eab2d8b26bf4f065f5
Jose Manuel Munoz <Neil_Mercury@yahoo.es>
2018-04-23 21:36:55 +0200

    Added labels to forms.py

--- librosBD/forms.py
+++ librosBD/forms.py
@@ -1,8 +1,23 @@
 from django import forms
 from .models import Book
 
+
+#class DateInput(forms.DateInput):
+#	input_type = 'date'
+
+
 class BookForm(forms.ModelForm):
 
 	class Meta:
 		model = Book
 		fields = ('title', 'author', 'start_reading_date', 'end_reading_date',)
+		labels = {
+			"title": "Título",
+			"author": "Autor",
+			"start_reading_date": "Fecha inicio de lectura",
+			"end_reading_date": "Fecha fin de lectura",
+		}
+		#widgets = {
+		#	"start_reading_date": DateInput(),
+		#	"end_reading_date": DateInput(),
+		#}
