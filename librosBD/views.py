d6337967e4ce7069db30bf9933492d9a37368c17
Jose Manuel Munoz <Neil_Mercury@yahoo.es>
2018-04-23 06:55:22 +0200

    Name/edit in book-author

--- librosBD/views.py
+++ librosBD/views.py
@@ -16,7 +16,7 @@ def book_detail(request, pk):
 def book_author(request, pk):
 	book = get_object_or_404(Book, pk=pk)
 	books = Book.objects.filter(author=book.author).order_by('title')
-	return render(request, 'librosBD/book_author.html', {'books': books})
+	return render(request, 'librosBD/book_author.html', {'books': books, 'author': book.author})
 
 
 def book_new(request):
